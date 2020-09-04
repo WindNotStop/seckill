fe471e701b862df2f1e19e54beb16d95f9900b3e
taenyang <1020560484@qq.com>
2020-09-04 17:22:18 +0800

    init

--- gateway/main.go
+++ gateway/main.go
@@ -13,7 +13,7 @@ import (
 var (
 	// command-line options:
 	// gRPC server endpoint
-	grpcServerEndpoint = flag.String("grpc-server-endpoint",  "localhost:9090", "gRPC server endpoint")
+	grpcServerEndpoint = flag.String("grpc-server-endpoint",  "seckill:9090", "gRPC server endpoint")
 )
 
 func run() error {
