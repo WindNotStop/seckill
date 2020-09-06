f1bbdace64f541f18023ed9b8b6883ea08d8a52a
WindNotStop <1020560484@qq.com>
2020-09-06 15:22:06 +0800

    init

--- seckill/config/config.go
+++ seckill/config/config.go
@@ -11,8 +11,8 @@ var(
 	GoodsName = "iphone"
 	GoodsNum = 10
 	loc, _ = time.LoadLocation("Asia/Shanghai")
-	StartTime = time.Date(2020, 9, 6, 14, 0, 0 ,0, loc)
-	EndTime = time.Date(2020, 9, 6, 15, 0, 0 ,0, loc)
+	StartTime = time.Date(2020, 9, 6, 15, 0, 0 ,0, loc)
+	EndTime = time.Date(2099, 9, 6, 15, 0, 0 ,0, loc)
 
 	FillInterval = time.Millisecond
 	Capacity int64 = 1000
